35c185e5be088a8f36f54333d2905a99713f8a8b
sjw000324@gmail.com <sjw000324@gmail.com>
2023-05-25 23:31:37 +0900

    플레이어 이동, 점프, 대시 구현

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,37 +5,130 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 10f;
-    public float jumpForce = 1f;
+    [SerializeField, Tooltip("기본 이동속도")] 
+    protected float speed;
+
+    [SerializeField, Tooltip("점프 입력 시 y축으로 받을 힘(Force)")] 
+    protected float jumpPower;
+
+    [SerializeField, Tooltip("대시 입력 시 x축으로 받을 힘(Force)")]  
+    protected float dashPower;
+
+    [SerializeField, Tooltip("대시 종료 후 재사용 대기시간")]  
+    protected float dashCooldown;
     
     private Rigidbody2D rig2D;
+    private Animator anim;
 
-    // Start is called before the first frame update
+    /* Basic Functions */
     void Start()
     {
         rig2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        move();
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetButtonDown("Dash"))
         {
-            jump();
+            if (!anim.GetBool("bDash"))
+            {
+                dash();
+            }
+        }
+        if (Input.GetButtonDown("Jump")) 
+        {
+            if (!anim.GetBool("bJump"))
+            {
+                jump();
+            }
+            else if (!anim.GetBool("bDoubleJump")) 
+            {
+                doubleJump();
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        checkJumpStatus();
+        if (!anim.GetBool("bDash"))
+        {
+            move();
         }
     }
 
     private void move()
     {
         float h = Input.GetAxis("Horizontal");
-
-        transform.position += new Vector3(h * speed * Time.deltaTime, 0, 0);
+        
+        transform.Translate(new Vector3(h * speed * Time.deltaTime, 0, 0));
     }
 
     private void jump()
     {
+        anim.SetBool("bJump", true);
         rig2D.velocity = new Vector2(0, 0);
-        rig2D.AddForce(new Vector2(0, jumpForce));
+        rig2D.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+    }
+
+    private void doubleJump()
+    {
+        anim.SetBool("bDoubleJump", true);
+        rig2D.velocity = new Vector2(0, 0);
+        rig2D.AddForce(new Vector2(0, jumpPower * 3f/4f), ForceMode2D.Impulse);
+    }
+
+    private void dash()
+    {
+        StartCoroutine(runDash());
+    }
+
+    private IEnumerator runDash()
+    {
+        anim.SetBool("bDash", true);
+        float unit = Input.GetAxisRaw("Horizontal");
+        if (unit == 0) yield break;
+
+        float s = dashPower;
+        while (7f < s)
+        {
+            yield return new WaitForFixedUpdate();
+            transform.position += new Vector3(unit * s * Time.deltaTime, 0, 0);
+            s -= 0.5f;
+        }
+
+        anim.SetBool("bDash", false);
+        yield break;
+    }
+
+    private bool isJump()
+    {
+        RaycastHit2D ray2D = Physics2D.Raycast(transform.position, Vector3.down, 1, LayerMask.GetMask("Ground"));
+
+        if(Mathf.Abs(rig2D.velocity.y) < 0.001f)
+        {
+            if(ray2D.collider != null)
+            {
+                if (ray2D.distance < 0.5f)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void checkJumpStatus()
+    {
+        if (isJump())
+        {
+            anim.SetBool("bJump", true);
+        }
+        else
+        {
+            anim.SetBool("bJump", false);
+            anim.SetBool("bDoubleJump", false);
+        }
     }
 }
