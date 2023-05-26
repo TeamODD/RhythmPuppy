5ecc8515db67d8f838b71c96c4bf829f8d3dd6c5
TRICKL4SH220 <small1258@naver.com>
2023-05-27 00:43:37 +0900

    feat : 메뉴 이동 효과음과 브금 추가

--- Assets/Scripts/scenechange/GotoStage1.cs
+++ Assets/Scripts/scenechange/GotoStage1.cs
@@ -1,24 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class GotoMenu : MonoBehaviour
-{   
-
+public class GotoStage1 : MonoBehaviour
+{
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("scenechange", 3);
+        
+    }
 
-    }
-    void scenechange()
-    {
-        SceneManager.LoadScene("SceneGameplay");
-    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
     }
 }
