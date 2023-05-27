7d7d914bbf581f1a2d0f23a7ee65ce1d463cfd20
kimms3920 <kimms3920@gmail.com>
2023-05-27 14:59:17 +0900

    feat : BGM와 효과음 조절 기능 추가

--- Assets/Scripts/Options/SoundOptions.cs
+++ Assets/Scripts/Options/SoundOptions.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public class SoundOptions : MonoBehaviour
 {
     public AudioSource musicsource;
+    public AudioSource btnsource;
     public void SetMusicVolume(float volume)
     {
         musicsource.volume = volume;
+    }
+
+    public void SetButtonVolume(float volume)
+    {
+        btnsource.volume = volume;
+    }
+
+    public void OnSfx()
+    {
+        btnsource.Play();
     }
 }
