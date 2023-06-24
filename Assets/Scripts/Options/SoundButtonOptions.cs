f7a62544832d1a92d5af65b199aec90d4e9da8a0
kimms3920 <kimms3920@gmail.com>
2023-06-24 21:06:52 +0900

    볼륨 숫자 표시

--- Assets/Scripts/Options/SoundButtonOptions.cs
+++ Assets/Scripts/Options/SoundButtonOptions.cs
@@ -1,6 +1,7 @@
 using UnityEngine.Audio;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SoundButtonOptions : MonoBehaviour
 {
@@ -8,30 +9,46 @@ public class SoundButtonOptions : MonoBehaviour
     public AudioSource btnsource;
     public Slider BGMSlider;
     public Slider SFSlider;
+    public TMP_Text BgmText;
+    public TMP_Text SfxText;
 
-    private float volumeIncrement = 0.1f; // 볼륨 증가량 조절   
+    private float volumeIncrement = 0.01f; // 볼륨 증가량 조절   
 
     public void IncreaseMusicVolume()
     {
         musicsource.volume = Mathf.Clamp01(musicsource.volume + volumeIncrement);
         BGMSlider.value = musicsource.volume;
+        UpdateMusicText();
     }
 
     public void DecreaseMusicVolume()
     {
         musicsource.volume = Mathf.Clamp01(musicsource.volume - volumeIncrement);
         BGMSlider.value = musicsource.volume;
+        UpdateMusicText();
     }
 
     public void IncreaseSfxVolume()
     {
         btnsource.volume = Mathf.Clamp01(btnsource.volume + volumeIncrement);
         SFSlider.value = btnsource.volume;
+        UpdateSfxText();
     }
 
     public void DecreaseSfxVolume()
     {
         btnsource.volume = Mathf.Clamp01(btnsource.volume - volumeIncrement);
         SFSlider.value = btnsource.volume;
+        UpdateSfxText();
+    }
+
+    private void UpdateMusicText()
+    {
+        BgmText.text = "Music : " + (musicsource.volume * 100f).ToString("0") + "%";
+    }
+
+    private void UpdateSfxText()
+    {
+        SfxText.text = "Sound : " + (btnsource.volume * 100f).ToString("0") + "%";
     }
 }
