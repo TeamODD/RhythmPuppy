602b4490d1b5ef8e3b21d9ed5ea0aeb2c10116ea
TRICKL4SH220 <small1258@naver.com>
2023-07-01 11:59:04 +0900

    feat : 메뉴창에서 플레이어의 이동과 스테이지 이동 구현

--- Assets/Scripts/Show Info.cs
+++ Assets/Scripts/Show Info.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShowArtist : MonoBehaviour
+public class ShowInfo : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
