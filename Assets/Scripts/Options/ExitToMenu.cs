24827172cf61ac4c67dd487c3250eb011ae784df
kimms3920 <kimms3920@gmail.com>
2023-07-03 18:00:35 +0900

    2023.07.03.18:00

---
+++ Assets/Scripts/Options/ExitToMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitToMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject Option;
+    [SerializeField] GameObject SceneMenu;
+    public AudioSource musicsource;
+
+    public void onExitToMenu()
+    {
+        Option.SetActive(false);
+        musicsource.Stop();
+
+        SceneManager.LoadScene("SceneMenu");
+    }
+}
