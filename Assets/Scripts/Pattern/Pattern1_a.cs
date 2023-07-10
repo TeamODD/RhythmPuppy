2924c678400cb1ffae30e550bf9811d1c201aebc
TRICKL4SH220 <small1258@naver.com>
2023-07-10 16:08:43 +0900

    feat : 제작 중

--- Assets/Scripts/Pattern/Pattern1_a.cs
+++ Assets/Scripts/Pattern/Pattern1_a.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Pattern1_a : MonoBehaviour
 {
     private Vector3 xPosition;
     private float yPosition;
 
+
     public void BeeMove()
     {
-        this.transform.position = new Vector3(14, 0, 0);
-        this.transform.position += new Vector3(-14, 0, -2);
+        
 
     }
 
