7b178a316ad4d5a048ffb036d6c1240cf5371d8f
kimms3920 <kimms3920@gmail.com>
2023-07-16 20:38:58 +0900

    6-α, 9, 10 패턴 완성

--- Assets/Scripts/Patterns/Pattern6.cs
+++ Assets/Scripts/Patterns/Pattern6.cs
@@ -11,57 +11,86 @@ public class Pattern6 : MonoBehaviour
     [SerializeField]
     private float stemSpeed = 3f;
 
+    private bool isPatternRunning = false;
+    private GameObject currentStem;
+
     private void OnEnable()
     {
-        StartCoroutine(MoveThornStem());
+        StartPattern();
     }
 
     private void OnDisable()
     {
-        StopCoroutine(MoveThornStem());
+        StopPattern();
     }
 
-    private IEnumerator MoveThornStem()
+    private void StartPattern()
     {
-        while (true)
+        if (!isPatternRunning)
         {
-            // 왼쪽 또는 오른쪽 위치에서 시작
-            float startX = Random.Range(0f, 1f) > 0.5f ? -7f : 7f;
-            float startY = Random.Range(0f, 1f) > 0.5f ? -2.5f : 2.5f;
-            Vector3 startPos = new Vector3(startX, startY, 0f);
-
-            // 경고 오브젝트 생성
-            Vector3 warningPosition = new Vector3(0.06f, startY, 0f);
-            GameObject warning = Instantiate(thornStemWarning, warningPosition, Quaternion.identity);
-
-            yield return new WaitForSeconds(0.5f);
-
-            // 가시 줄기 생성
-            GameObject stem = Instantiate(thornStem, startPos, Quaternion.identity);
-            Rigidbody2D stemRigidbody = stem.GetComponent<Rigidbody2D>();
-
-            // 경고 오브젝트 제거
-            Destroy(warning);
-
-            // 오른쪽으로 이동
-            if (startX < 0f)
-                stemRigidbody.velocity = Vector2.right * stemSpeed;
-            // 왼쪽으로 이동
-            else
-                stemRigidbody.velocity = Vector2.left * stemSpeed;
-
-
-            StartCoroutine(DestroyIfOutOfBounds(stem));
+            isPatternRunning = true;
+            StartCoroutine(RunPattern());
         }
     }
+
+    private void StopPattern()
+    {
+        isPatternRunning = false;
+        if (currentStem != null)
+        {
+            Destroy(currentStem);
+            currentStem = null;
+        }
+        StopAllCoroutines();
+    }
+
+    private IEnumerator RunPattern()
+    {
+        while (isPatternRunning)
+        {
+            if (currentStem == null) // 현재 생성된 장애물이 없을 때만 패턴 실행
+            {
+                // 왼쪽 또는 오른쪽 위치에서 시작
+                float startX = Random.Range(0f, 1f) > 0.5f ? -10f : 10f;
+                float startY = 0;
+                Vector3 startPos = new Vector3(startX, startY, 0f);
+
+                // 경고 오브젝트 생성
+                Vector3 warningPosition = new Vector3(startX, startY, 0f);
+                GameObject warning = Instantiate(thornStemWarning, warningPosition, Quaternion.identity);
+
+                yield return new WaitForSeconds(0.5f);
+
+                // 가시 줄기 생성
+                currentStem = Instantiate(thornStem, startPos, Quaternion.identity);
+                Rigidbody2D stemRigidbody = currentStem.GetComponent<Rigidbody2D>();
+
+                // 경고 오브젝트 제거
+                Destroy(warning);
+
+                // 오른쪽으로 이동
+                if (startX < 0f)
+                    stemRigidbody.velocity = Vector2.right * stemSpeed;
+                // 왼쪽으로 이동
+                else
+                    stemRigidbody.velocity = Vector2.left * stemSpeed;
+
+                StartCoroutine(DestroyIfOutOfBounds(currentStem));
+            }
+
+            yield return null;
+        }
+    }
+
     private IEnumerator DestroyIfOutOfBounds(GameObject obj)
     {
-        while (true)
+        while (isPatternRunning)
         {
             // 맵 밖으로 나갈 경우 오브젝트를 파괴합니다.
             if (!IsWithinMapBounds(obj.transform.position))
             {
                 Destroy(obj);
+                currentStem = null;
                 yield break;
             }
             yield return null;
---
+++ Assets/Scripts/Patterns/Pattern8a.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pattern8a : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject weasel;
+    [SerializeField]
+    private GameObject weaselWarning;
+    [SerializeField]
+    private float weaselSpeed = 10f;
+    [SerializeField]
+    private float[] rhythmTimings = { 0f, 0.6f, 0.8f, 1.1f, 1.5f, 1.8f, 2.2f, 2.3f, 2.7f, 2.9f, 3.2f, 3.5f, 3.9f };
+
+    private Coroutine weaselCoroutine;
+    private GameObject currentWarning;
+
+    private void OnEnable()
+    {
+        StartPattern();
+    }
+
+    private void OnDisable()
+    {
+        StopPattern();
+    }
+
+    private void StartPattern()
+    {
+        weaselCoroutine = StartCoroutine(WeaselRoutine());
+    }
+
+    private void StopPattern()
+    {
+        if (weaselCoroutine != null)
+        {
+            StopCoroutine(weaselCoroutine);
+            weaselCoroutine = null;
+        }
+
+        if (currentWarning != null)
+        {
+            Destroy(currentWarning);
+            currentWarning = null;
+        }
+    }
+
+    private IEnumerator WeaselRoutine()
+    {
+        float startTime = Time.time;
+
+        for (int i = 0; i < rhythmTimings.Length; i++)
+        {
+            float targetTime = startTime + rhythmTimings[i];
+
+            while (Time.time < targetTime)
+            {
+                yield return null;
+            }
+
+            if (i < rhythmTimings.Length - 1) // 마지막 족제비 타이밍 이전에만 경고 표시
+            {
+                if (currentWarning != null)
+                {
+                    Destroy(currentWarning);
+                    currentWarning = null;
+                }
+
+                Vector3 spawnPosition = ShowWeaselWarning();
+                yield return new WaitForSeconds(0.5f);
+
+                Destroy(currentWarning);
+                yield return StartCoroutine(SpawnWeasel(spawnPosition)); // 변경된 부분
+            }
+            else // 마지막 족제비 타이밍에 모든 경고 표시 제거
+            {
+                if (currentWarning != null)
+                {
+                    Destroy(currentWarning);
+                    currentWarning = null;
+                }
+            }
+        }
+    }
+
+    private Vector3 ShowWeaselWarning()
+    {
+        float xPos = Random.Range(-9f, 9f);
+        Vector3 warningPosition = new Vector3(xPos, -4.6f, 0f);
+        currentWarning = Instantiate(weaselWarning, warningPosition, Quaternion.identity);
+        return new Vector3(xPos, -5f, 0f);
+    }
+
+    private IEnumerator SpawnWeasel(Vector3 spawnPosition)
+    {
+        GameObject newWeasel = Instantiate(weasel, spawnPosition, Quaternion.identity);
+        Rigidbody2D weaselRigidbody = newWeasel.GetComponent<Rigidbody2D>();
+        weaselRigidbody.velocity = Vector2.up * weaselSpeed;
+
+        yield return new WaitForSeconds(0.5f);
+
+        weaselRigidbody.velocity = Vector2.down * weaselSpeed;
+
+        yield return new WaitForSeconds(0.5f);
+
+        StartCoroutine(DestroyIfOutOfBounds(newWeasel));
+    }
+
+    private IEnumerator DestroyIfOutOfBounds(GameObject obj)
+    {
+        while (true)
+        {
+            // 맵 밖으로 나갈 경우 오브젝트를 파괴합니다.
+            if (!IsWithinMapBounds(obj.transform.position))
+            {
+                Destroy(obj);
+                yield break;
+            }
+            yield return null;
+        }
+    }
+
+    private bool IsWithinMapBounds(Vector3 position)
+    {
+        float minX = -10f;
+        float maxX = 10f;
+        float minY = -5f;
+        float maxY = 5f;
+
+        return position.x >= minX && position.x <= maxX && position.y >= minY && position.y <= maxY;
+    }
+}
