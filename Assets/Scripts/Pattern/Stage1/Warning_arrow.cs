970803f8b1ba1710d8b354d42167d4b7db927168
TRICKL4SH220 <small1258@naver.com>
2023-08-02 16:37:44 +0900

    feat : 스테이지1 그래픽 적용 작업 완료

--- Assets/Scripts/Pattern/Stage1/Warning_arrow.cs
+++ Assets/Scripts/Pattern/Stage1/Warning_arrow.cs
@@ -2,28 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Warning3 : MonoBehaviour
+public class Warning_arrow : MonoBehaviour
 {
-    public static float xPosition;
     private float time;
-    void Awake()
-    {
-        xPosition = Random.Range(-8.5f, 8.5f);
-    }
+    // Start is called before the first frame update
     void Start()
     {
         time = 0;
-        gameObject.transform.position = new Vector3(xPosition, -4f, 0);
         GetComponent<SpriteRenderer>().color = new Color(1, 0.3f, 0.3f, 0);
 
     }
+
+    // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
         if (time < 0.5f)
             GetComponent<SpriteRenderer>().color = new Color(1, 0.3f, 0.3f, time / 1f);
         else
-            GetComponent<SpriteRenderer>().color = new Color(1, 0.3f, 0.3f, 1f-time / 1f);
+            GetComponent<SpriteRenderer>().color = new Color(1, 0.3f, 0.3f, 1f - time / 1f);
 
         //1초후 gameObject 삭제
         Destroy(gameObject, 1f);
