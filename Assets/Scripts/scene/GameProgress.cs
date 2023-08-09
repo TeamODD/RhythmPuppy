b078c09bd3cf5d14260029933b6fe711a0368d91
kimms3920 <kimms3920@gmail.com>
2023-08-09 16:56:53 +0900

    세이프포인트 관련 별 기능 완성

--- Assets/Scripts/scene/GameProgress.cs
+++ Assets/Scripts/scene/GameProgress.cs
@@ -20,6 +20,11 @@ public class GameProgress : MonoBehaviour
     private float musicLength;
     private Vector3 initialPlayerBudgePosition;
     private float targetDistance; // 이동할 거리
+    private bool isarrivecheckpoint1 = false;
+    private bool isarrivecheckpoint2 = false;
+    private bool isarrivecheckpoint3 = false;
+
+    public float checkpoint;
 
     private void Start()
     {
@@ -45,7 +50,7 @@ public class GameProgress : MonoBehaviour
         }
 
         MovePlayerBudge(currentMusicPosition);
-        SavePointChecking();
+        SavePointChecking(fillAmount);
     }
 
     private void MovePlayerBudge(float currentMusicPosition)
@@ -62,8 +67,79 @@ public class GameProgress : MonoBehaviour
         }
     }
 
-    private void SavePointChecking()
+    private void SavePointChecking(float fillAmount)
     {
-        
+        float checkpoint1Threshold = 0.25f; // 25% 세이브 포인트 임계값
+        float checkpoint2Threshold = 0.50f; // 50% 세이브 포인트 임계값
+        float checkpoint3Threshold = 0.75f; // 75% 세이브 포인트 임계값
+
+        // 25% 지점 체크
+        if (fillAmount >= checkpoint1Threshold)
+        {
+            emptysavepoint1.SetActive(false);
+            fullsavepoint1.SetActive(true);
+            isarrivecheckpoint1 = true;
+        }
+        else
+        {
+            emptysavepoint1.SetActive(true);
+            fullsavepoint1.SetActive(false);
+        }
+
+        // 50% 지점 체크
+        if (fillAmount >= checkpoint2Threshold)
+        {
+            emptysavepoint2.SetActive(false);
+            fullsavepoint2.SetActive(true);
+            isarrivecheckpoint2 = true;
+        }
+        else
+        {
+            emptysavepoint2.SetActive(true);
+            fullsavepoint2.SetActive(false);
+        }
+
+        // 75% 지점 체크
+        if (fillAmount >= checkpoint3Threshold)
+        {
+            emptysavepoint3.SetActive(false);
+            fullsavepoint3.SetActive(true);
+            isarrivecheckpoint3 = true;
+        }
+        else
+        {
+            emptysavepoint3.SetActive(true);
+            fullsavepoint3.SetActive(false);
+        }
+    }
+
+    private void BacktoCheckPoint()
+    {
+        float checkpointTime = 0f;
+
+        if (isarrivecheckpoint1)
+        {
+            checkpointTime = musicLength * 0.25f;
+        }
+        else if (isarrivecheckpoint2)
+        {
+            checkpointTime = musicLength * 0.50f;
+        }
+        else if (isarrivecheckpoint3)
+        {
+            checkpointTime = musicLength * 0.75f;
+        }
+
+        // 플레이어 뱃지의 위치 설정
+        float normalizedPosition = Mathf.Clamp01(checkpointTime / musicLength);
+        float targetX = Mathf.Lerp(initialPlayerBudgePosition.x, initialPlayerBudgePosition.x + targetDistance, normalizedPosition);
+        Vector3 newPosition = new Vector3(targetX, initialPlayerBudgePosition.y, initialPlayerBudgePosition.z);
+        playerbudge.transform.position = newPosition;
+
+        // 음악 재생 위치 설정
+        musicAudioSource.time = checkpointTime;
+
+        // fillImage 업데이트 (필요한 경우)
+        fillImage.fillAmount = checkpointTime / musicLength;
     }
 }
