c32d8174ebc78406d99fbe6b17085b03009b966a
kimms3920 <kimms3920@gmail.com>
2023-08-16 23:38:11 +0900

    스테이지1-3 패턴13제작, 다만 수정이 필요해보임.

--- Assets/Scripts/stage1-2 Patterns/PatternControllerrrrrr.cs
+++ Assets/Scripts/stage1-2 Patterns/PatternControllerrrrrr.cs
@@ -188,7 +188,7 @@ public class PatternControllerrrrrr : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log("GetElapsedTime : " + GetElapsedTime());
+        //Debug.Log("GetElapsedTime : " + GetElapsedTime());
     }
 
     private IEnumerator RunPattern6()
