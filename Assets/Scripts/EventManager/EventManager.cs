07fd3a7d84846891ae73731db3d9aa8f7eed9364
sjw000324@gmail.com <sjw000324@gmail.com>
2024-02-27 16:25:00 +0900

    feat : 마우스 커서 및 Player(메뉴씬) Sleeping 동작 추가

--- Assets/Scripts/EventManager/EventManager.cs
+++ Assets/Scripts/EventManager/EventManager.cs
@@ -7,17 +7,10 @@ namespace EventManagement
 {
     public class EventManager : MonoBehaviour
     {
-        public UnityEvent pauseEvent;
-
         [HideInInspector]
         public float[] savePointTime;
         public StageEvent stageEvent;
         public PlayerEvent playerEvent;
         public UIEvent uiEvent;
-
-        void Start()
-        {
-            pauseEvent.Invoke();
-        }
     }
 }
--- Assets/Scripts/Player/AFK.cs
+++ Assets/Scripts/Player/AFK.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AFK : MonoBehaviour
 {
-    public Animator animator;
-    public Sprite normal;
-    public Sprite Sleeping;
-    public Sprite WakeUp;
-    private float lastInputTime;
+    [SerializeField] SpriteRenderer head;
+    [SerializeField] Transform noseBubble;
+    [SerializeField] Transform blownBubble;
+    [SerializeField] Sprite normalFace;
+    [SerializeField] Sprite sleepyFace;
+    [SerializeField] Sprite wakeupFace;
 
-    void Start()
+    Animator animator;
+    float lastInputTime;
+    WaitForSeconds wakeupFaceDelay;
+
+    void Awake()
     {
-        
+        animator = GetComponent<Animator>();
+        wakeupFaceDelay = new WaitForSeconds(0.667f);
+        blownBubble.gameObject.SetActive(false);    // Only Activated on WakeUp Animation 
     }
 
     // Update is called once per frame
@@ -22,14 +30,35 @@ public class AFK : MonoBehaviour
         {
             // 입력이 감지되면 현재 시간으로 갱신
             lastInputTime = Time.time;
-            animator.SetBool("IsAFK", false);
+
+            // 만약 Sleep 동작 중이었다면
+            if (animator.GetBool("IsAFK"))
+            {
+                animator.SetBool("IsAFK", false);
+                blownBubble.gameObject.SetActive(true);
+                StartCoroutine(wakeUpCoroutine());
+
+            }
         }
 
-        // 특정 시간(5초) 동안 입력이 없으면 특정 동작 수행
+        // 특정 시간(3초) 동안 입력이 없으면 특정 동작 수행
         if (Time.time - lastInputTime > 3f)
         {
             // 여기에 수행할 동작을 추가
+            head.sprite = sleepyFace;
             animator.SetBool("IsAFK", true);
         }
+    }
+
+    IEnumerator wakeUpCoroutine()
+    {
+        // While Running Wakeup Animator
+        head.sprite = wakeupFace;
+        yield return wakeupFaceDelay;
+
+        // After Wakeup Animator 
+        blownBubble.gameObject.SetActive(false);
+        noseBubble.localScale = Vector3.zero;
+        head.sprite = normalFace;
     }
 }
